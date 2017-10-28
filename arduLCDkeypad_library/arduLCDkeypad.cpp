b91dfc0cc1412d9f229a1cdd7e5ad93ef6a8fc1c
mackelec <mackelec@users.noreply.github.com>
2017-10-28 12:19:44 +1000

    Add files via upload

---
+++ arduLCDkeypad_library/arduLCDkeypad.cpp
@@ -0,0 +1,203 @@
+
+#include "arduLCDkeypad.h"
+
+arduLCDkeypad::arduLCDkeypad(Stream &dev)
+{
+  comms = &dev;
+}
+
+arduLCDkeypad::arduLCDkeypad(Stream &dev,byte add)
+{
+  comms = &dev;
+  address = add;
+}
+
+void arduLCDkeypad::clear()
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "Clr" << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::setCursor(int col,int row)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "C" << DELIM << col << DELIM << row << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::Contrast(int percent)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "Contrast" << DELIM << percent << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::BackLight(int percent_standby,int percent_active)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "BackLight" << DELIM << percent_standby << DELIM << percent_active << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::BackLightTimer(int backlightTimeout)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "BLt" << DELIM << backlightTimeout << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::Buzz(int buzzTime)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "Bz" << DELIM << buzzTime << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::printAt(const char message[],uint8_t col,uint8_t row,uint8_t blank)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "PA" << DELIM << message << DELIM << col << DELIM << row << DELIM << blank << endl;
+  sendPrintCommand(buff);
+  
+}
+void arduLCDkeypad::printAt(const char message[],uint8_t col,uint8_t row)
+{
+  printAt(message,col,row,0);
+}
+void arduLCDkeypad::updateLcdField(uint8_t id,uint8_t col,uint8_t row,uint8_t len,bool visible,char strFormat[])
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "UF" << DELIM << id << DELIM << col << DELIM << row << DELIM << len << DELIM << visible <<   DELIM << strFormat << endl;
+  sendPrintCommand(buff);
+}
+void arduLCDkeypad::updateLcdFieldFormat(uint8_t id, char strFormat[])
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "Uf" << DELIM << id << DELIM << strFormat << endl;
+  sendPrintCommand(buff);
+}
+void arduLCDkeypad::updateStrContent(uint8_t id,char strContent[])
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff << "US" << DELIM << id << DELIM << strContent << endl; 
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::updateTime(unsigned long _time)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff  << F("UT") << DELIM << _time << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::setAutoTime(int8_t fieldNum)
+{
+  PString pBuff(buff,sizeof(buff));
+  pBuff  << F("SAT") << DELIM << fieldNum << endl;
+  sendPrintCommand(buff);
+}
+
+void arduLCDkeypad::sendPrintCommand(char message[])
+{
+  if (address > 0)
+  {
+    Wire.beginTransmission(address);
+    Wire.write(message);
+    Wire.endTransmission(); 
+    //Serial << "write *mess : " << message ;  
+  }
+  else
+  {
+    comms->print(message);
+  }
+}
+
+char arduLCDkeypad::keyPressed()
+{
+  char key = 0;
+  if (address > 0)
+  {
+    key = keyPressed_I2C();
+  }
+  else
+  {
+    key = keyPressed_Serial();
+  }
+  return key;
+}
+
+bool arduLCDkeypad::recReset()
+{
+  if (receivedReset)
+  {
+    receivedReset = false;
+    return true; 
+  }
+  return false;
+}
+  
+bool arduLCDkeypad::present() {return alive;}
+
+char arduLCDkeypad::keyPressed_I2C()
+{
+  Wire.requestFrom(42, 7);
+  char key = checkKeydata();
+  return key;
+}
+
+char arduLCDkeypad::checkKeydata()
+{
+  char buff[8];
+  int ptr = 0;
+  char retvalue = 0;
+  
+  alive = false;
+  while (Wire.available())  
+  {
+    char c = Wire.read(); 
+    if (c == '\n') 
+    {
+      buff[ptr]=0;
+      int _reset = strncmp("reset",buff,5);
+      if (_reset == 0) 
+      {
+        receivedReset = true;
+        alive = true;
+      }
+      int cmp = strncmp("key=",buff,4);
+      if (cmp==0 )
+      {
+        alive = true;
+        if ( buff[4] != i2cNoKey)  
+        {
+          retvalue = buff[4]; 
+        }
+        return retvalue; 
+      }
+    }
+    buff[ptr] = c;
+    ptr++;
+  }
+  return retvalue;
+}
+
+
+char arduLCDkeypad::keyPressed_Serial()
+{
+  PString pBuff(buff,sizeof(buff));
+  char retvalue = 0;
+  while (comms->available()>0)
+  {
+    char c = comms->read();
+    if (c == '\n') break;
+    pBuff += c;  
+  } 
+  int cmp = strncmp("key=",buff,4);
+  if (cmp==0 && buff[4] != i2cNoKey) 
+  {
+    retvalue = buff[4]; 
+  }
+  return retvalue; 
+}
+
---
+++ arduLCDkeypad_library/arduLCDkeypad.h
@@ -0,0 +1,146 @@
+#ifndef arduLCDkeypad_H
+#define arduLCDkeypad_H
+
+#if !defined(__time_t_defined) // avoid conflict with newlib or other posix libc
+typedef unsigned long time_t;
+#endif
+
+
+#include <Streaming.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <PString.h>
+
+#define DELIM       '~'
+#define NO_KEY       0
+#define i2cNoKey    '!'
+
+
+class arduLCDkeypad 
+{
+public:
+  arduLCDkeypad(Stream &dev);
+  arduLCDkeypad(Stream &dev,byte add);
+  
+  void printAt(const char message[],uint8_t col,uint8_t row);
+  void printAt(const char message[],uint8_t col,uint8_t row,uint8_t blank);
+  void clear();
+  void setCursor(int col,int row);
+  void Contrast(int percent);
+  void BackLight(int percent_standby,int percent_active);
+  void BackLightTimer(int backlightTimeout);
+  void Buzz(int buzzTime);
+  char keyPressed();
+  bool recReset();
+  bool present();
+  void updateLcdField(uint8_t id,uint8_t col,uint8_t row,uint8_t len,bool visible,char strFormat[]);
+  void updateLcdFieldFormat(uint8_t id, char strFormat[]);
+  void updateStrContent(uint8_t id,char strContent[]);
+  void updateTime(unsigned long _time);
+  void setAutoTime(int8_t fieldNum);
+  
+/**
+*    Prints functions
+**/
+
+  template <class ANY>
+  void Prints(ANY data[])
+  {
+    PString pBuff(buff,sizeof(buff));
+    pBuff << "P" << DELIM << data << endl;
+    sendPrintCommand(buff);
+  }
+  
+  template <class ANY>
+  void Prints(ANY data)
+  {
+    PString pBuff(buff,sizeof(buff));
+    pBuff << "P" << DELIM << data << endl;
+    sendPrintCommand(buff);
+  }
+   
+  //template <typename ANY>
+  union varToBytes
+  {
+    uint8_t uint8_value;
+    int8_t int8_value;
+    unsigned int uint_value;
+    int int_value;
+    unsigned long ulong_value;
+    long long_value;
+    float float_value;
+    byte bytes[4];
+  };
+
+/***
+*      send an Update value to LCD
+*
+***/
+   
+   template <class ANY>
+   void updateValue(uint8_t id, ANY data)
+   {
+     uint8_t itype = varType(data);
+     varToBytes u;
+     u.ulong_value = 0;
+     switch (itype)
+     {
+       case 2:  u.uint8_value = data;  break;
+       case 3:  u.int8_value = data;  break;
+       case 4:  u.uint_value = data;  break;
+       case 5:  u.int_value = data; break;
+       case 6:  u.ulong_value = data; break;
+       case 7:  u.long_value = data; break;
+       case 8:  u.float_value = data; break;
+     }
+     char strValue[24];
+     PString pvalue(strValue,sizeof(strValue));
+     pvalue << "US" << DELIM << id << DELIM << itype << DELIM << data;
+     if (itype > 1)
+     {
+       pvalue.begin();
+       pvalue << "UV" << DELIM << id << DELIM << itype;
+       for (int i=0;i<4;i++)
+       {
+         pvalue << DELIM << _HEX(u.bytes[i]) ;
+       }
+     }
+     pvalue << endl;
+     //Serial << "Update Value : " << pvalue <<  endl;
+     sendPrintCommand(strValue);
+     //Serial << "vartype = " << itype << endl;
+   }
+   
+   
+   
+  int8_t varType(char var){return 0;}
+  int8_t varType(char var[]){return 1;}
+  int8_t varType(uint8_t var){return 2;}
+  int8_t varType(int8_t var){return 3;}
+  int8_t varType(unsigned int var){return 4;}
+  int8_t varType(int var){return 5;}
+  int8_t varType(unsigned long var){return 6;}
+  int8_t varType(long var){return 7;}
+  int8_t varType(float var){return 8;}
+
+  
+
+  
+private:
+  Stream *comms;
+  byte address = 0;
+  bool receivedReset = false;
+  bool alive = false;
+  char buff[48];
+  char checkKeydata();
+  char keyPressed_I2C();
+  char keyPressed_Serial();
+  void sendPrintCommand(char message[]);
+  
+  
+};
+
+
+#endif
+
+//    http://arduiniana.org/libraries/pstring/
